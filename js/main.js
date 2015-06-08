a9fa80f83a5875e73abbf8557ed147ebc19f73cd
Amy Pellegrini <vicesaraco@gmail.com>
2015-06-08 15:33:06 -0300

    Contact form handling.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+var App = (function() {
+  var contactBtn = document.getElementById('navContact'),
+      contactForm = document.getElementById('contact'),
 
+      hideContactForm = function(ev) {
+        ev.stopPropagation();
+
+        if (ev.target === ev.currentTarget) {
+          contactForm.className = "email form overlay container hidden";
+        }
+      },
+
+      showContactForm = function() {
+        contactForm.className = "email form overlay container";
+      };
+
+  contactBtn.addEventListener('click', showContactForm);
+  contactForm.addEventListener('click', hideContactForm);
+}());
