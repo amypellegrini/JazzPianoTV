e3f89347b93ef29189da08eb4b90dd835196fffb
Amy Pellegrini <vicesaraco@gmail.com>
2015-08-20 04:04:38 -0300

    Exercise controller implementation.

---
+++ js/controllers/exercise-controller.js
@@ -0,0 +1,12 @@
+define([], function() {
+  /**
+   * Exercise Controller Class definition.
+   * Searches the entire App context for exercises and proceeds
+   * to initialize them.
+   */
+  var ExerciseController = function() {
+    return {};
+  };
+
+  return ExerciseController;
+});
