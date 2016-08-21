e99fc6171f244290f22dd587064b320db1097433
amyvpellegrini@gmail.com <amyvpellegrini@gmail.com>
2016-08-21 17:59:11 +0100

    Main app refactor.

---
+++ js/app.js
@@ -0,0 +1,12 @@
+(function() {
+  angular
+    .module('JazzPianoTV')
+    .controller('MainController', MainController);
+
+  /**
+   * Main app controller.
+   */
+  function MainController() {
+
+  }
+})();
