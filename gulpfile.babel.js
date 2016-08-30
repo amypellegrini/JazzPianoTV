8a9fde5312c0889d94084c7c578b58e359f1474e
Amy Pellegrini <amy.pellegrini@wowcher.co.uk>
2016-08-30 17:28:16 +0100

    Fix default gulp tast / build index with injected deps :)

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,18 +2,19 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const inject = require('gulp-inject');
 
-// Desired production build steps: concat > minify > inject
+// Desired production build steps: concat > minify > inject > serve
 // Desired dev build steps: inject > serve
 
 gulp.task('default', () => {
-  var target = gulp.src('./src/index.html');
-  var sources = gulp.src(['./src/**/*.js', './src/**/*.css'], { read: false });
+  let target = gulp.src('./src/index.html');
+  let sources = gulp.src(['./src/**/*.js', './src/**/*.css'], { read: false });
 
+  // @todo: replace harcoded babel config by the real one (load, etc)
+  // @todo: stream babel task for js files
   return target
     .pipe(inject(sources))
-    // @todo: replace harcoded babel config by the real one (load, etc)
-    .pipe(babel({
-      presets: ['es2015']
-    }))
-    .pipe(gulp.dest('./dist'));
+    // .pipe(babel({
+    //   presets: ['es2015']
+    // }))
+    .pipe(gulp.dest('./dist/'));
 });
