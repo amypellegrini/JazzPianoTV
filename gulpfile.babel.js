d9f497a742173841e446f1c25253255db5542dc9
Amy Pellegrini <amy.pellegrini@wowcher.co.uk>
2016-08-30 16:47:38 +0100

    Rename gulpfile to enable gulp with babel/es2015.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,14 +1,19 @@
 const gulp = require('gulp');
+const babel = require('gulp-babel');
 const inject = require('gulp-inject');
 
 // Desired production build steps: concat > minify > inject
 // Desired dev build steps: inject > serve
 
-gulp.task('index', function() {
+gulp.task('default', () => {
   var target = gulp.src('./src/index.html');
   var sources = gulp.src(['./src/**/*.js', './src/**/*.css'], { read: false });
 
   return target
     .pipe(inject(sources))
-    .pipe(gulp.dest('./src'));
+    // @todo: replace harcoded babel config by the real one (load, etc)
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(gulp.dest('./dist'));
 });
