c458bc24fa9ddc92bdbaccea70229683a5ca8452
Amy Pellegrini <amy.pellegrini@wowcher.co.uk>
2016-08-31 11:26:42 +0100

    Build index with html/css injects basic gulp task (sort of).

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,19 +2,30 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const inject = require('gulp-inject');
 
-// Desired production build steps: concat > minify > inject > serve
-// Desired dev build steps: inject > serve
+/**
+ * Desired production build steps: concat > bebel > minify > inject > serve
+ *
+ * Ideally we sohuld be able to use sourcemaps for debugging with client side
+ * code minified and bundled in its final state, no need for a "dev" or "prod"
+ * build.
+ */
+gulp.task('css', () => {
+  let src = gulp.src('./src/**/*.css');
+  return src.pipe(gulp.dest('dist'));
+});
 
-gulp.task('default', () => {
+gulp.task('js', () => {
+  let src = gulp.src('./src/**/*.js');
+  return src
+    .pipe(babel({ presets: ['es2015'] }))
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('default', ['js', 'css'], () => {
   let target = gulp.src('./src/index.html');
-  let sources = gulp.src(['./src/**/*.js', './src/**/*.css'], { read: false });
+  let sources = gulp.src(['./dist/**/*.js', './dist/**/*.css'], { read: false });
 
-  // @todo: replace harcoded babel config by the real one (load, etc)
-  // @todo: stream babel task for js files
   return target
-    .pipe(inject(sources))
-    // .pipe(babel({
-    //   presets: ['es2015']
-    // }))
+    .pipe(inject(sources, { relative: false }))
     .pipe(gulp.dest('./dist/'));
 });
