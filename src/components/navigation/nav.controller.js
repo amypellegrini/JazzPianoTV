389a53f863c893220076ef93f7acaaadca80e6a3
Amy Pellegrini <amy.pellegrini@wowcher.co.uk>
2016-09-02 17:00:42 +0100

    Add nav menu exercieses option.

--- src/components/navigation/nav.controller.js
+++ src/components/navigation/nav.controller.js
@@ -2,6 +2,7 @@ class NavController {
   constructor() {
     this.items = [
       { text: 'Blog', href: '/blog' },
+      { text: 'Exercises', href: '/exercises' },
       { text: 'Login/Register', href: '/register-login' },
       { text: 'About', href: '/about' }
     ];
