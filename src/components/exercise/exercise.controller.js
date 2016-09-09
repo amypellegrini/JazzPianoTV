8ad675675c4b959b072f359ebf5bc385a3e810bf
amyvpellegrini@gmail.com <amyvpellegrini@gmail.com>
2016-09-10 00:15:17 +0100

    Refactor exercise component to support left hand/right hand images.

--- src/components/exercise/exercise.controller.js
+++ src/components/exercise/exercise.controller.js
@@ -1,7 +1,12 @@
 class ExerciseController {
+  /**
+   * @param {Object} $scope
+   * @constructor
+   */
   constructor($scope) {
     this.score = 0;
-    this.path = './../media/voicings/357-735-573/a-c-amaj7.svg';
+    this.rhPath = 'https://s3-eu-west-1.amazonaws.com/jazzpianotv/assets/voicings/357-735-573/a-c-amaj7.svg';
+    this.lhPath = "";
   }
 }
 
