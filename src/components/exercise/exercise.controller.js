17bdaaf9045baf7ae7e016c3207e806bde205ea9
amyvpellegrini@gmail.com <amyvpellegrini@gmail.com>
2016-09-10 11:30:26 +0100

    Render exercise layout / add some styles.

--- src/components/exercise/exercise.controller.js
+++ src/components/exercise/exercise.controller.js
@@ -4,9 +4,36 @@ class ExerciseController {
    * @constructor
    */
   constructor($scope) {
+    let rootUrl = 'https://s3.amazonaws.com/jazzpiano.tv/assets/';
+
     this.score = 0;
-    this.rhPath = 'https://s3-eu-west-1.amazonaws.com/jazzpianotv/assets/voicings/357-735-573/a-c-amaj7.svg';
-    this.lhPath = "";
+
+    this.sequence = [
+      {
+        key: 'C',
+        tempo: 60,
+        timeSignature: 'C',
+        rh: rootUrl + 'voicings/c-c-cmaj7.svg',
+        lh: rootUrl + 'left-hand/c-c-c.svg',
+        hFunction: 'I'
+      },
+      {
+        key: 'C',
+        tempo: 60,
+        timeSignature: 'C',
+        rh: rootUrl + 'voicings/c-c-dm7.svg',
+        lh: rootUrl + 'left-hand/c-c-d.svg',
+        hFunction: 'II'
+      },
+      {
+        key: 'C',
+        tempo: 60,
+        timeSignature: 'C',
+        rh: rootUrl + 'voicings/c-c-g7.svg',
+        lh: rootUrl + 'left-hand/c-c-g.svg',
+        hFunction: 'V'
+      }
+    ];
   }
 }
 
