1a1b9288736397c16ded9e1e2f74101e91e55f21
amyvpellegrini@gmail.com <amyvpellegrini@gmail.com>
2017-05-07 08:49:38 +0100

    New order notes test.

--- src/order-notes/order-notes.test.js
+++ src/order-notes/order-notes.test.js
@@ -1,12 +1,22 @@
 import test from 'tape';
 import orderNotes from './order-notes';
 
-test('Order notes function', (assert) => {
-  const message = 'Given an array of notes, it should return an array'
+test('order notes function', (assert) => {
+  const message = 'given an array of notes, it should return an array'
     + 'with the same notes in ascending order';
 
   const expected = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
   const actual = orderNotes(['B', 'A', 'D', 'E', 'C', 'G', 'F']);
+
+  assert.deepEqual(actual, expected, message);
+  assert.end();
+});
+
+test('order notes repeated values', (assert) => {
+  const message = 'it should be able to handle repeated values';
+
+  const expected = ['A', 'B', 'B', 'C', 'C', 'C', 'F'];
+  const actual = orderNotes(['B', 'C', 'F', 'B', 'C', 'A', 'C']);
 
   assert.deepEqual(actual, expected, message);
   assert.end();
