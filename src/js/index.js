6f59c69ddc51872f28e0d291b2902042e9106839
Amy Pellegrini <amyvpellegrini@gmail.com>
2017-07-12 23:54:25 +0100

    Implement home default route.

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,13 @@
-import '../css/main.css';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { render } from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  browserHistory } from 'react-router-dom';
+
+import '../css/main.css';
 import jptvApp from './reducers/jptv-app';
 import createApp from './App';
 
@@ -11,7 +16,9 @@ let store = createStore(jptvApp);
 
 render(
   <Provider store={ store }>
-    <App />
+    <Router history={ browserHistory }>
+      <Route exact path="/" component={ App }/>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
