0aa3a7ebbe3711c82e0a00a25439145fb71ea07e
amyvpellegrini@gmail.com <amyvpellegrini@gmail.com>
2017-09-18 23:10:50 +0100

    Start nav menu implementation.

---
+++ src/js/components/nav-menu/nav-menu.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+import './nav-menu.css';
+
+export default React => () => {
+  return (
+    <nav>
+      <ul className="jptv-nav-menu">
+        <li>Lessons</li>
+      </ul>
+    </nav>
+  );
+}
---
+++ src/js/components/nav-menu/nav-menu.test.js
@@ -0,0 +1,16 @@
+import test from 'tape';
+import React from 'react';
+import dom from 'cheerio';
+import reactDom from 'react-dom/server';
+
+import createNavMenu from './nav-menu';
+
+const render = reactDom.renderToStaticMarkup;
+
+test('Navigation Menu component', nest => {
+  nest.test('rendering', assert => {
+    const message = 'It should render the navigation menu.';
+
+   //  const 
+  });
+});
