0d86f4e2543228955800b70f77e92f4efb7ece56
amyvpellegrini@gmail.com <amyvpellegrini@gmail.com>
2017-09-27 23:11:10 +0100

    App layout refactor.

--- src/js/App.js
+++ src/js/App.js
@@ -3,20 +3,41 @@ import createLoginMenu from './containers/login-menu/login-menu';
 import createNavMenu from './components/nav-menu/nav-menu';
 import createFooter from './components/footer/footer';
 
+import createSignup from './components/signup/signup';
+import createMyAccount from './containers/my-account/my-account';
+import createLogin from './containers/login/login';
+import {
+  BrowserRouter as Router,
+  Route,
+  browserHistory } from 'react-router-dom';
+
 export default React => () => {
   const Header = createHeader(React);
   const LoginMenu = createLoginMenu(React);
   const NavMenu = createNavMenu(React);
   const Footer = createFooter(React);
+  const Signup = createSignup(React);
+  const Login = createLogin(React);
+  const MyAccount = createMyAccount(React);
 
   return (
-    <section id="jptv">
-      <header>
-        <Header />
-        <NavMenu />
-        <LoginMenu />
-      </header>
-      <Footer />
-    </section>
+    <Router history={ browserHistory }>
+      <section id="jptv">
+        <header>
+          <Header />
+          <NavMenu />
+          <LoginMenu />
+        </header>
+
+        <Route path="/signup"
+               component={ Signup } />
+        <Route path="/login"
+               component={ Login } />
+        <Route path="/my-account"
+               component={ MyAccount } />
+
+        <Footer />
+      </section>
+    </Router>
   );
 };
--- src/js/index.js
+++ src/js/index.js
@@ -11,9 +11,6 @@ import {
 import '../css/main.css';
 import jptvApp from './reducers/jptv-app';
 import createApp from './App';
-import createSignup from './components/signup/signup';
-import createMyAccount from './containers/my-account/my-account';
-import createLogin from './containers/login/login';
 
 // Polyfill for older browsers that do not support ES6 promise.
 if (!window.Promise) {
@@ -23,18 +20,12 @@ if (!window.Promise) {
 let store = createStore(jptvApp);
 
 const App = createApp(React);
-const Signup = createSignup(React);
-const Login = createLogin(React);
-const MyAccount = createMyAccount(React);
 
 render(
   <Provider store={ store }>
     <Router history={ browserHistory }>
       <div>
         <Route path="/" component={ App }/>
-        <Route path="/signup" component={ Signup } />
-        <Route path="/login" component={ Login } />
-        <Route path="/my-account" component={ MyAccount } />
       </div>
     </Router>
   </Provider>,
