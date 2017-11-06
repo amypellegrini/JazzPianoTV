9d398f45a79a4cb89404f4c945025beaf8ec5167
amyvpellegrini@gmail.com <amyvpellegrini@gmail.com>
2017-11-06 18:39:33 +0000

    Refactor HTML / move navigation to header.

--- src/js/App.js
+++ src/js/App.js
@@ -1,11 +1,10 @@
 import createHeader from './components/header/header';
-import createLoginMenu from './containers/login-menu/login-menu';
 import createNavMenu from './components/nav-menu/nav-menu';
 import createFooter from './components/footer/footer';
-
 import createSignup from './components/signup/signup';
 import createMyAccount from './containers/my-account/my-account';
 import createLogin from './containers/login/login';
+
 import {
   BrowserRouter as Router,
   Route,
@@ -13,7 +12,6 @@ import {
 
 export default React => () => {
   const Header = createHeader(React);
-  const LoginMenu = createLoginMenu(React);
   const NavMenu = createNavMenu(React);
   const Footer = createFooter(React);
   const Signup = createSignup(React);
@@ -25,7 +23,6 @@ export default React => () => {
       <section id="jptv">
         <Header />
         <NavMenu />
-        <LoginMenu />
 
         <Route path="/signup"
                component={ Signup } />
--- src/js/components/header/header.js
+++ src/js/components/header/header.js
@@ -1,16 +1,19 @@
 import createLogo from '../logo/logo';
 import createQuote from '../quote/quote';
 import createSearch from '../search/search';
+import createLoginMenu from '../login-menu/login-menu.container';
 
 export default React => () => {
   const Logo = createLogo(React);
   const Quote = createQuote(React);
   const Search = createSearch(React);
+  const LoginMenu = createLoginMenu(React);
 
   return (
     <header>
       <Logo />
       <Quote />
+      <LoginMenu />
       <Search />
     </header>
   );
