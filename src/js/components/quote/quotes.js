8a426a769ac044aa42c0eb37f8bea53ebe9d3680
amyvpellegrini@gmail.com <amyvpellegrini@gmail.com>
2017-11-26 17:07:10 +0000

    Quote component refactor.

--- src/js/components/quote/quotes.js
+++ src/js/components/quote/quotes.js
@@ -1,3 +1,9 @@
-export default [
-  "The height of the key is all you have to make the difference between noise and music",
-];
+export default {
+  quotes: [
+    "The height of the key is all you have to make the difference between noise and music.",
+  ],
+  subtitles: [
+    "Learn to Play the Piano for real",
+    "The framework for the professional pianist"
+  ]
+};
