4de793d452746f87267ddd032650fb1e8c8127ae
amyvpellegrini@gmail.com <amyvpellegrini@gmail.com>
2017-12-14 16:31:48 +0000

    Add HPTM short pharagraph description.

--- src/js/components/landing-page/landing-page.js
+++ src/js/components/landing-page/landing-page.js
@@ -8,6 +8,9 @@ export default React => () => {
           <h2>Coming soon</h2>
           <p>The Hanon Pilgrimage to Mastery</p>
         </header>
+        <p>Being a talented person is not enough. Understanding technique is not enough. Being disciplined, if not balanced with acute awareness, inner clarity, emotional stability and deep understanding of technique, is not gonna cut it.</p>
+        <p><strong>The Hanon Pilgrimage to Mastery</strong> is designed to help you develop the traits you need to achieve Piano mastery and virtuosity.</p>
+        <hr />
         <p>Looking for sheet music? Feel free to check out Amy Pellegrini <a className="jptv-musescore-link" href="https://musescore.com/user/3607356" target="_blank">Musescore Profile</a>.
         </p>
       </section>
