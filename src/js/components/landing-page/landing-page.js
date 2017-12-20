b1bdf7548bf3c8d6a0074b1eb2a50b42c8c930cb
amyvpellegrini@gmail.com <amyvpellegrini@gmail.com>
2017-12-20 18:50:01 +0000

    Change landing page text.

--- src/js/components/landing-page/landing-page.js
+++ src/js/components/landing-page/landing-page.js
@@ -8,8 +8,8 @@ export default React => () => {
           <h2>The Hanon Pilgrimage to Mastery</h2>
           <p>Coming soon</p>
         </header>
-        <p>Being a talented person is not enough. Understanding technique is not enough.</p>
-        <p>Being disciplined, if not balanced with acute awareness, inner clarity, emotional stability, and deep understanding of technique, is not gonna cut it.</p>
+        <p>Being talented is not enough. Understanding technique is not enough.</p>
+        <p>Being disciplined, if not balanced with acute awareness, inner clarity, emotional stability, and deep understanding of technique, is not going to cut it.</p>
         <p><strong>The Hanon Pilgrimage to Mastery</strong> is designed to help you develop the traits you need to achieve Piano mastery and virtuosity.</p>
         <hr />
         <p>Looking for sheet music? Feel free to check out Amy Pellegrini <a className="jptv-musescore-link" href="https://musescore.com/user/3607356" target="_blank">Musescore Profile</a>.
