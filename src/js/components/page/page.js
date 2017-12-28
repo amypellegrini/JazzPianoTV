4d56860438b66431bb04da5815489db2d146888c
amyvpellegrini@gmail.com <amyvpellegrini@gmail.com>
2017-12-28 04:25:48 +0000

    Page renderer component (init render & loading).

---
+++ src/js/components/page/page.js
@@ -0,0 +1,20 @@
+export default React => {
+  class Page extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { loading: true };
+    }
+
+    render() {
+      const loadingClass = this.state.loading ? "loading" : "";
+      const classNames = [ 
+          "jptv-page-container",
+          loadingClass
+        ];
+
+      return <div className={ classNames.join(' ') }></div>;
+    }
+  }
+
+  return Page;
+}
---
+++ src/js/components/page/page.test.js
@@ -0,0 +1,70 @@
+import test from 'tape';
+import React from 'react';
+import reactDom from 'react-dom/server';
+import dom from 'cheerio';
+
+import createPage from './page';
+
+const render = reactDom.renderToStaticMarkup;
+
+test('Page component', nest => {
+  nest.test('. rendering', assert => {
+    const message = 'It should render a given page.';
+
+    const matchProp = {
+      params: {
+        pageId: 'jptv-12-rules'
+      }
+    };
+
+    const Page = createPage(React);
+    const $ = dom.load(render(<Page match={ matchProp } />));
+    const output = $('.jptv-page-container');
+
+    const actual = output.length > 0;
+    const expected = true;
+
+    assert.equal(actual, expected, message);
+    assert.end();
+  });
+
+  nest.test('. loading state', assert => {
+    const message = 'Initial state should be "loading" until page renders.';
+
+    const matchProp = {
+      params: {
+        pageId: 'jptv-12-rules'
+      }
+    };
+
+    const Page = createPage(React);
+    const $ = dom.load(render(<Page match={ matchProp } />));
+    const output =  $('.jptv-page-container');
+
+    const actual = output.hasClass('loading');
+    const expected = true;
+
+    assert.equal(actual, expected, message);
+    assert.end();
+  });
+
+  nest.test('. page content async', assert => {
+    const message = 'It should load and render a given page by ID.'
+
+    const matchProp = {
+      params: {
+        pageId: 'jptv-12-rules'
+      }
+    };
+
+    const Page = createPage(React);
+    const $ = dom.load(render(<Page match={ matchProp } />));
+    const output = $('h2').text();
+
+    const actual = output;
+    const expected = 'JazzPianoTV 12 Rules for Piano Mastery';
+
+    assert.equal(actual, expected, message);
+    assert.end();
+  });
+});
