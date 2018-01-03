c0fbe3e770c4020d7a11ba1bba396899ce86745f
amyvpellegrini@gmail.com <amyvpellegrini@gmail.com>
2018-01-03 23:51:48 +0000

    Remove unused code.

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import { render } from 'react-dom';
-import {
-  BrowserRouter as Router,
-  Route,
-  browserHistory } from 'react-router-dom';
 
 import '../css/main.css';
-import jptvApp from './reducers/jptv-app';
 import createApp from './App';
-
-let store = createStore(jptvApp);
 
 const App = createApp(React);
 
