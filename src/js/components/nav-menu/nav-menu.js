907c7af9c4ac43c0c8f8f9990ea8409d80a30d38
amyvpellegrini@gmail.com <amyvpellegrini@gmail.com>
2018-01-03 23:09:53 +0000

    Remove nav options / prepare for integration.

--- src/js/components/nav-menu/nav-menu.js
+++ src/js/components/nav-menu/nav-menu.js
@@ -7,9 +7,6 @@ export default React => () => {
     <nav>
       <ul className="jptv-nav-menu">
         <li>
-          <Link to="/shows">Shows</Link>
-        </li>
-        <li>
           <Link to="/exercises">Exercises</Link>
         </li>
         <li>
@@ -17,15 +14,6 @@ export default React => () => {
         </li>
         <li>
           <Link to="/lessons">Lessons/Articles</Link>
-        </li>
-        <li>
-          <Link to="/videos">Videos</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-        <li>
-          <Link to="/products">Products</Link>
         </li>
         <li>
           <Link to="/about">About</Link>
