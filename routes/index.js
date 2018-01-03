cd0b7d851c764a9994310777dc2fbe8dd4f3dca5
amyvpellegrini@gmail.com <amyvpellegrini@gmail.com>
2018-01-03 23:05:43 +0000

    Remove old/unused route.

--- routes/index.js
+++ routes/index.js
@@ -7,22 +7,5 @@ module.exports = function(passport) {
     res.render('index', { title: 'Express' });
   });
 
-  router.post('/signup', function(req, res, next) {
-    passport.authenticate('local-signup', { session: false }, function(err, user, info) {
-      if (err) {
-        return next(err);
-      }
-      if (!user) {
-        return res.json(info);
-      }
-      req.logIn(user, function(err) {
-        if (err) {
-          return next(err);
-        }
-        return res.json(info);
-      });
-    })(req, res, next);
-  });
-
   return router;
 }
