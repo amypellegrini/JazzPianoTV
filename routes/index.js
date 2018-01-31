4ef59986f6e02516ff28d8d0e9446f5ec022deb0
amyvpellegrini@gmail.com <amyvpellegrini@gmail.com>
2018-01-31 14:38:37 +0000

    Server render (header/footer).

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
+require('babel-register');
+require('ignore-styles');
+
+var React = require('react');
+var reactDOMServer = require('react-dom/server');
 var express = require('express');
 var router = express.Router();
+var createApp = require('../src/js/app-server').default;
 
 module.exports = function(passport) {
+  var app = createApp(React);
+  var render =  reactDOMServer.renderToString(app());
+
   /* GET home page. */
   router.get('/*', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { serverRender: render });
   });
 
   return router;
