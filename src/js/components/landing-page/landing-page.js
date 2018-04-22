ef1307e9e1c1b84ebd656b3a123c07092e0b0802
amyvpellegrini@gmail.com <amyvpellegrini@gmail.com>
2018-04-22 03:58:39 +0100

    Wording change.

--- src/js/components/landing-page/landing-page.js
+++ src/js/components/landing-page/landing-page.js
@@ -12,12 +12,13 @@ export default React => () => {
         <p>Being disciplined, if not balanced with acute awareness, inner clarity, emotional stability, and deep understanding of technique, is not going to cut it.</p>
 
         <p><strong>JazzPianoTV</strong> is designed to help you develop the traits you need to achieve Piano mastery and virtuosity.</p>
-
+      </section>
+      <section>
         <h3>How does it work?</h3>
 
         <p><strong>JazzPianoTV</strong> contents are intended to be practiced in conjunction with the <Link to="/jptv-12-rules" className="jptv-12-rules-link" label="JazzPianoTV 12 Rules for Piano Mastery"/>, as a framework for personal and technical development of Piano Mastery.</p>
 
-        <p>We could think about <strong>The Hanon Pilgrimage to Mastery</strong>, <strong>The Book of Triads</strong>, or <strong>A Crush Course on JazzPiano</strong> as the road to travel, and the <strong>JazzPianoTV 12 Rules for Piano Mastery</strong> as the guideline to make sure you are well prepared for the trip.</p>
+        <p>We could think about <strong>The Hanon Pilgrimage to Mastery</strong>, <strong>The Book of Triads</strong>, or <strong>A Crush Course on JazzPiano</strong> courses as the road to travel, and the <strong>JazzPianoTV 12 Rules for Piano Mastery</strong> as the guideline to make sure you are well prepared for the trip.</p>
 
         <p>Are you ready?</p>
       </section>
